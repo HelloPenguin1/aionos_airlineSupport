ed6996f53e1b5386e4909657820f5962cc69871b
Soham Datta <dattasoham05@gmail.com>
2026-09-17 22:54:35 +0530

    fixed retriever

---
+++ rag/retriever.py
@@ -0,0 +1,29 @@
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+
+CHROMA_PATH = "rag/chroma_db"
+COLLECTION_NAME = "service_rules"
+
+
+def get_retriever(rules: list[str]):
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="BAAI/bge-base-en-v1.5"
+    )
+
+    vectorstore = Chroma(
+        collection_name=COLLECTION_NAME,
+        embedding_function=embeddings,
+        persist_directory=CHROMA_PATH
+    )
+
+    return vectorstore.as_retriever(
+        search_kwargs={
+            "k": len(rules),
+            "filter": {
+                "rule": {
+                    "$in": rules
+                }
+            }
+        }
+    )
